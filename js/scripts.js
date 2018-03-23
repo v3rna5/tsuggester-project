a5507b8cb912dbdacae17689c32d3b11f39ebdaa
Verna Santos <Guest@epicodus-20.local>
2018-03-23 15:49:07 -0700

    Troubleshoot form

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,32 @@ $(document).ready(function() {
     var react = $("select#reactproject").val();
     var csharp = $("select#cproject").val();
     var javascript = $("select#jproject").val();
-  //var nameinput = $("input#name").val();
+    var nameinput = $("input#name1").val();
 
-    //blanks.forEach(function(blank) {
-      //var userInput = $("input." + blank).val();
-      //$("." + blank) .text(userInput).val();
-    //});
+
 
     if (javascript === "Most Likely") {
+
       $(".languages #javascriptinfo").toggle();
       $(".languages #cinfo").hide();
       $(".languages #reactinfo").hide();
-    } else {
-      alert("please see");
     }
+
     if (react === "Most Likely") {
         $(".languages #reactinfo").toggle();
         $(".languages #cinfo").hide();
         $(".languages #javascriptinfo").hide();
-      } else {
-        alert("please see");
       }
+
     if (csharp === "Most Likely") {
       $(".languages #cinfo").toggle();
       $(".languages #reactinfo").hide();
       $(".languages #javascriptinfo").hide();
-    } else {
-      alert("please see");
-    }
-    if (javascript === "Most Likely" || react === "Most Likely" || csharp === "Most Likely") {
-      $(".languages").show();
 
     }
-
+    if (javascript === "Most Likely" + react ==="Most Likely" + csharp ==="Most Likely") {
+      alert("Please visit our store");
+    }
 
 
     event.preventDefault();
