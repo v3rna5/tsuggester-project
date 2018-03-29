860b779c14c4b6b999b90d14aad5fdc569120fc9
vernajs <vernajs@gmail.com>
2018-03-28 22:45:19 -0700

    Fixed - blank alert, populate name on each result

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,51 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    //var blanks = ["inputhobby", "SelectC", "SelectJavascript", "SelectReact", "firstlast" ]
+
     var react = $("select#reactproject").val();
     var csharp = $("select#cproject").val();
     var javascript = $("select#jproject").val();
-    //var nameinput = $("input#name1").val();
-    var blanks = ["cnameinput"];
-    blanks.forEach(function(blank) {
-    var userInput = $("input.name" + blank).val();
-          $("." + blank).text(userInput).val();
-        });
-
-        alert("Please enter name");
+    var blank = $("input#name1").val();
+    var blank2 = $("input#hobby").val();
 
 
+
+    if (blank === "") {
+      alert("Please enter name!!");
+      ("form").reset()
+    } else {
+      $(".languages #cnameinput").text($("input#name1").val());
+    }
+
+
+    if (blank2 === "") {
+      alert("Please enter hobby!!");
+      ("form").reset()
+    }
     if (javascript === "Most Likely") {
       $(".languages #javascriptinfo").toggle();
       $(".languages #cinfo").hide();
       $(".languages #reactinfo").hide();
     } else {
-    $(".languages #information").show();
+      $(".languages #information").show();
+
     }
     if (react === "Most Likely") {
-        $(".languages #reactinfo").toggle();
-        $(".languages #cinfo").hide();
-        $(".languages #javascriptinfo").hide();
-      }else {
+      $(".languages #reactinfo").toggle();
+      $(".languages #cinfo").hide();
+      $(".languages #javascriptinfo").hide();
+    }else {
+      $(".languages #information").show();
 
-            $(".languages #information").show();
-      }
-
+    }
     if (csharp === "Most Likely") {
       $(".languages #cinfo").toggle();
       $(".languages #reactinfo").hide();
       $(".languages #javascriptinfo").hide();
-
     } else {
+      $(".languages #information").show();
 
-          $(".languages #information").show();
     }
-
 
     event.preventDefault();
   });
-});
+  });
